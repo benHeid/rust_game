38244bc4a4489db845eb35ffbede0c79d1eb9c0a
benHeid <benHeid@users.noreply.github.com>
2019-04-08 14:26:19 +0200

    use circle instead of boxes

--- src/bin/dragons.rs
+++ src/bin/dragons.rs
@@ -1,3 +1,5 @@
+#![no_std]
+
 use libm;
 use rand::prelude::*;
 use rand::Rng;
@@ -13,24 +15,33 @@ pub static COLORS: [(u8, u8, u8); 5] = [
 ];
 
 #[derive(Copy, Clone)]
+pub struct Circle {
+    pub radius: u16,
+    pub pos: Vector2d,
+    pub vel: Vector2d,
+    pub col: Color,
+}
+
+#[derive(Copy, Clone)]
 pub struct Box {
     pub size: u16,
     pub pos: Vector2d,
     pub vel: Vector2d,
     pub col: Color,
 }
+
 #[derive(Copy, Clone)]
-pub struct Dragon(pub Box);
+pub struct Dragon(pub Circle);
 
 impl core::ops::Deref for Dragon {
-    type Target = Box;
-    fn deref(&self) -> &Box {
+    type Target = Circle;
+    fn deref(&self) -> &Circle {
         &self.0
     }
 }
 
 impl core::ops::DerefMut for Dragon {
-    fn deref_mut(&mut self) -> &mut Box {
+    fn deref_mut(&mut self) -> &mut Circle {
         &mut self.0
     }
 }
@@ -40,29 +51,174 @@ impl Dragon {
         &mut self,
         layer: &mut stm32f7_discovery::lcd::Layer<stm32f7_discovery::lcd::FramebufferArgb8888>,
     ) {
-        for y in 0..self.size {
-            for x in 0..self.size {
-                let i = 20 + x as usize + self.pos.x as usize;
-                let j = 20 + y as usize + self.pos.y as usize;
-                let wert = IMG[2 * (x + y * 30) as usize + 1];
-                if wert > 25 {
+        for y in 0 ..2* self.radius {
+            for x in 0.. 2 * self.radius {
+                let i = 20 + x as usize + self.pos.x as usize - self.radius as usize;
+                let j = 20 + y as usize + self.pos.y as usize - self.radius as usize;
+                let value = IMG[2 * (x + y * 30) as usize + 1];
+                if value > 25 {
                     layer.print_point_color_at(i, j, self.col)
                 }
             }
         }
     }
+    pub fn derender(
+        &mut self,
+        layer: &mut stm32f7_discovery::lcd::Layer<stm32f7_discovery::lcd::FramebufferArgb8888>,
+        bg_color: Color,
+    ) {
+        for i in (20 + self.pos.x)..(20 + self.pos.x + 2* self.radius as i16) {
+            for j in (20 + self.pos.y)..(20 + self.pos.y + 2* self.radius as i16) {
+                layer.print_point_color_at(i as usize - self.radius as usize, j as usize - self.radius as usize, bg_color)
+            }
+        }
+    }
+
     pub fn hit(&mut self, hit: Vector2d, background_col: &mut Color) -> (bool, bool) {
-        if self.pos.x + 20 <= hit.x && hit.x <= self.pos.x + self.size as i16 + 20 {
-            //set derender color of object
-            if self.pos.y + self.size as i16 + 20 >= hit.y && hit.y >= self.pos.y + 20 {
+        if i32::from(hit.x - self.pos.x - 20)  * i32::from(hit.x - self.pos.x - 20) +
+        i32::from(hit.y - self.pos.y - 20)* i32::from(hit.y - self.pos.y - 20) 
+            < i32::from(self.radius) * i32::from(self.radius)
+            {
                 if self.col.to_rgb() == background_col.to_rgb() {
                     return (true, true);
                 } else {
                     return (true, false);
                 }
             }
-        }
         (false, false)
+    }
+}
+
+impl Circle {
+    pub fn new(radius: u16, x: i16, y: i16, vel_x: i16, vel_y: i16, vel_col: Color) -> Self {
+        Self {
+            radius,
+            pos: Vector2d { x, y },
+            vel: Vector2d { x: vel_x, y: vel_y },
+            col: vel_col,
+        }
+    }
+    // pub fn render(
+    //     &mut self,
+    //     layer: &mut stm32f7_discovery::lcd::Layer<stm32f7_discovery::lcd::FramebufferArgb8888>,
+    // ) {
+    //     for i in (0)..= self.radius as i16 {
+    //         for j in (0)..= self.radius as i16 {
+    //             if i * i + j * j <= (self.radius * self.radius) as i16 {
+    //                 layer.print_point_color_at(20 + self.pos.x as usize + i as usize, 20 + self.pos.y as usize + j as usize, self.col);
+    //                 layer.print_point_color_at(20 + self.pos.x as usize - i as usize, 20 + self.pos.y as usize + j as usize, self.col);
+    //                 layer.print_point_color_at(20 + self.pos.x as usize + i as usize, 20 + self.pos.y as usize - j as usize, self.col);
+    //                 layer.print_point_color_at(20 + self.pos.x as usize - i as usize, 20 + self.pos.y as usize - j as usize, self.col);
+    //             }
+    //         }
+    //     }
+    // }
+    //  pub fn derender(
+    //     &mut self,
+    //     layer: &mut stm32f7_discovery::lcd::Layer<stm32f7_discovery::lcd::FramebufferArgb8888>,
+    //     bg_color: Color,
+    // ) {
+    //     for i in (0)..=(0 + self.radius as i16) {
+    //         for j in (0)..=(0 + self.radius as i16) {
+    //             if i * i + j * j <= (self.radius * self.radius) as i16 {
+    //                 layer.print_point_color_at(20 + self.pos.x as usize + i as usize, 20 + self.pos.y as usize + j as usize, bg_color);
+    //                 layer.print_point_color_at(20 + self.pos.x as usize - i as usize, 20 + self.pos.y as usize + j as usize, bg_color);
+    //                 layer.print_point_color_at(20 + self.pos.x as usize + i as usize, 20 + self.pos.y as usize - j as usize, bg_color);
+    //                 layer.print_point_color_at(20 + self.pos.x as usize - i as usize, 20 + self.pos.y as usize - j as usize, bg_color);
+    //             }
+    //         }
+    //     }
+    // }
+
+    pub fn collision(&self, c: &Circle) -> Option<Vector2d> {
+        if self.intersect(c) {
+            let normal_vector = Vector2d {
+                x: self.pos.x - c.pos.x,
+                y: self.pos.y - c.pos.y,
+            };
+            let inner_product_vel =
+                (self.vel.x - c.vel.x) * normal_vector.x + (self.vel.y - c.vel.y) * normal_vector.y;
+            let squared_dist =
+                normal_vector.x * normal_vector.x + normal_vector.y * normal_vector.y;
+
+            Some(Vector2d {
+                x: self.vel.x
+                    - libm::ceilf(
+                        f32::from(inner_product_vel) / f32::from(squared_dist)
+                            * f32::from(normal_vector.x),
+                    ) as i16,
+                y: self.vel.y
+                    - libm::ceilf(
+                        f32::from(inner_product_vel) / f32::from(squared_dist)
+                            * f32::from(normal_vector.y),
+                    ) as i16,
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn intersect(&self, b: &Circle) -> bool {
+        if self.pos.x == b.pos.x
+            && self.pos.y == b.pos.y
+            && self.vel.x == b.vel.x
+            && self.vel.y == b.vel.y
+            && self.radius == b.radius
+        {
+            return false;
+        }
+
+        i32::from(b.pos.x - self.pos.x + b.vel.x - self.vel.x)  * i32::from(b.pos.x - self.pos.x + b.vel.x - self.vel.x) +
+        i32::from(b.pos.y - self.pos.y + b.vel.y - self.vel.y)* i32::from(b.pos.y - self.pos.y + b.vel.y - self.vel.y)
+            < i32::from(b.radius + self.radius) * i32::from(b.radius + self.radius) + 9
+    }
+
+        pub fn update_speed(&mut self, new_speed: Vector2d) {
+            self.vel = new_speed;
+            if self.vel.x > 10 {
+                self.vel.x = 10;
+            }
+            if self.vel.y > 10 {
+                self.vel.y = 10;
+            }
+             if self.vel.x < -10 {
+                self.vel.x = -10;
+            }
+            if self.vel.y < -10 {
+                self.vel.y = -10;
+            }
+        }
+
+        pub fn next(&mut self) {
+            let height = 252;
+            let width = 460;
+            let offset = 20;
+            if self.radius as i16 + self.pos.x as i16 + self.vel.x > width - offset {
+                self.vel.x *= -1;
+            }
+            if self.pos.x as i16 - self.radius as i16 + self.vel.x < 20 {
+                self.vel.x *= -1;
+            }
+            if self.radius as i16 + self.pos.y as i16 + self.vel.y > height - offset{
+                self.vel.y *= -1;
+            }
+            if self.pos.y as i16 - self.radius  as i16 + self.vel.y < 20 {
+                self.vel.y *= -1;
+            }
+            self.pos.x += self.vel.x;
+            self.pos.y += self.vel.y;
+        }
+    pub fn random(rand: &mut StdRng) -> Self {
+        let tupel = COLORS[rand.gen_range(0, 4)];
+        let color = Color::rgb(tupel.0, tupel.1, tupel.2);
+        Self::new(
+            15,
+            rand.gen_range(60, 400),
+            rand.gen_range(60, 200),
+            rand.gen_range(-10, 10),
+            rand.gen_range(-10, 10),
+            color,
+        )
     }
 }
 
@@ -75,29 +231,13 @@ impl Box {
             col: vel_col,
         }
     }
-    pub fn random(rand: &mut StdRng) -> Self {
-        let tupel = COLORS[rand.gen_range(0, 4)];
-        let color = Color::rgb(tupel.0, tupel.1, tupel.2);
-        Self::new(
-            30,
-            rand.gen_range(60, 400),
-            rand.gen_range(60, 200),
-            rand.gen_range(-10, 10),
-            rand.gen_range(-10, 10),
-            color,
-        )
-    }
     pub fn render(
         &mut self,
         layer: &mut stm32f7_discovery::lcd::Layer<stm32f7_discovery::lcd::FramebufferArgb8888>,
     ) {
         for i in (20 + self.pos.x)..=(20 + self.pos.x + self.size as i16) {
             for j in (20 + self.pos.y)..=(20 + self.pos.y + self.size as i16) {
-                layer.print_point_color_at(
-                    i as usize,
-                    j as usize,
-                    self.col,
-                )
+                layer.print_point_color_at(i as usize, j as usize, self.col)
             }
         }
     }
@@ -114,7 +254,7 @@ impl Box {
         }
     }
 
-    pub fn hit(&mut self, hit: &Vector2d) -> bool {
+    pub fn hit(&mut self, hit: Vector2d) -> bool {
         if self.pos.x + 20 <= hit.x && hit.x <= self.pos.x + self.size as i16 + 20 {
             //set derender color of object
             self.col = Color::from_hex(0x00ff_ffff);
@@ -125,75 +265,83 @@ impl Box {
         false
     }
 
-    pub fn collision(&self, b: &Box) -> Option<Vector2d> {
-        if self.intersect(b) {
-            let normal_vector = Vector2d {
-                x: self.pos.x - b.pos.x,
-                y: self.pos.y - b.pos.y,
-            };
-            let inner_product_vel =
-                (self.vel.x - b.vel.x) * normal_vector.x + (self.vel.y - b.vel.y) * normal_vector.y;
-            let squared_dist =
-                normal_vector.x * normal_vector.x + normal_vector.y * normal_vector.y;
+    // pub fn collision(&self, b: &Box) -> Option<Vector2d> {
+    //     if self.intersect(b) {
+    //         let normal_vector = Vector2d {
+    //             x: self.pos.x - b.pos.x,
+    //             y: self.pos.y - b.pos.y,
+    //         };
+    //         let inner_product_vel =
+    //             (self.vel.x - b.vel.x) * normal_vector.x + (self.vel.y - b.vel.y) * normal_vector.y;
+    //         let squared_dist =
+    //             normal_vector.x * normal_vector.x + normal_vector.y * normal_vector.y;
 
-            Some(Vector2d {
-                x: self.vel.x
-                    - libm::ceilf(
-                        f32::from(inner_product_vel) / f32::from(squared_dist) * f32::from(normal_vector.x),
-                    ) as i16,
-                y: self.vel.y
-                    - libm::ceilf(
-                        f32::from(inner_product_vel) / f32::from(squared_dist) * f32::from(normal_vector.y),
-                    ) as i16,
-            })
-        } else {
-            None
-        }
-    }
+    //         Some(Vector2d {
+    //             x: self.vel.x
+    //                 - libm::ceilf(
+    //                     f32::from(inner_product_vel) / f32::from(squared_dist)
+    //                         * f32::from(normal_vector.x),
+    //                 ) as i16,
+    //             y: self.vel.y
+    //                 - libm::ceilf(
+    //                     f32::from(inner_product_vel) / f32::from(squared_dist)
+    //                         * f32::from(normal_vector.y),
+    //                 ) as i16,
+    //         })
+    //     } else {
+    //         None
+    //     }
+    // }
 
-    pub fn intersect(&self, b: &Box) -> bool {
-        if self.pos.x == b.pos.x
-            && self.pos.y == b.pos.y
-            && self.vel.x == b.vel.x
-            && self.vel.y == b.vel.y
-            && self.size == b.size
-        {
-            return false;
-        }
+    // pub fn intersect(&self, b: &Box) -> bool {
+    //     if self.pos.x == b.pos.x
+    //         && self.pos.y == b.pos.y
+    //         && self.vel.x == b.vel.x
+    //         && self.vel.y == b.vel.y
+    //         && self.size == b.size
+    //     {
+    //         return false;
+    //     }
 
-        ((self.pos.x - b.pos.x + self.vel.x -b.vel.x).abs() < (self.size as i16 - 3))
-            && ((self.pos.y - b.pos.y + self.vel.y - b.vel.y).abs() < (self.size as i16 - 3))
-    }
+    //     ((self.pos.x - b.pos.x + self.vel.x - b.vel.x).abs() < (self.size as i16 - 3))
+    //         && ((self.pos.y - b.pos.y + self.vel.y - b.vel.y).abs() < (self.size as i16 - 3))
+    // }
 
-    pub fn update_speed(&mut self, new_speed: &Vector2d) {
-        self.vel = *new_speed;
-        if self.vel.x > 10 {
-            self.vel.x = 10;
-        }
-        if self.vel.y > 10 {
-            self.vel.y = 10;
-        }
-    }
+    //     pub fn update_speed(&mut self, new_speed: &Vector2d) {
+    //         self.vel = *new_speed;
+    //         if self.vel.x > 10 {
+    //             self.vel.x = 10;
+    //         }
+    //         if self.vel.y > 10 {
+    //             self.vel.y = 10;
+    //         }
+    //          if self.vel.x < -10 {
+    //             self.vel.x = -10;
+    //         }
+    //         if self.vel.y < -10 {
+    //             self.vel.y = -10;
+    //         }
+    //     }
 
-    pub fn next(&mut self) {
-        let height = 252;
-        let width = 460;
-        let offset = 20;
-        if offset + self.size as i16 + self.pos.x as i16 + self.vel.x > width {
-            self.vel.x *= -1;
-        }
-        if offset + self.pos.x as i16 + self.vel.x < 20 {
-            self.vel.x *= -1;
-        }
-        if offset + self.size as i16 + self.pos.y as i16 + self.vel.y > height {
-            self.vel.y *= -1;
-        }
-        if offset + self.pos.y as i16 + self.vel.y < 20 {
-            self.vel.y *= -1;
-        }
-        self.pos.x += self.vel.x;
-        self.pos.y += self.vel.y;
-    }
+    //     pub fn next(&mut self) {
+    //         let height = 252;
+    //         let width = 460;
+    //         let offset = 20;
+    //         if offset + self.size as i16 + self.pos.x as i16 + self.vel.x > width {
+    //             self.vel.x *= -1;
+    //         }
+    //         if offset + self.pos.x as i16 + self.vel.x < 20 {
+    //             self.vel.x *= -1;
+    //         }
+    //         if offset + self.size as i16 + self.pos.y as i16 + self.vel.y > height {
+    //             self.vel.y *= -1;
+    //         }
+    //         if offset + self.pos.y as i16 + self.vel.y < 20 {
+    //             self.vel.y *= -1;
+    //         }
+    //         self.pos.x += self.vel.x;
+    //         self.pos.y += self.vel.y;
+    //     }
 }
 
 impl PartialEq for Box {
